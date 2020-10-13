b9d018113d865b7026df2d2c4140528aef74d366
Fadel Hakim <fadel.lukmanh@gmail.com>
2020-10-13 18:24:13 +0700

    Add project files.

---
+++ ArchivingFiles/ManageFolder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ArchivingFiles
+{
+    public class ManageFolder
+    {
+        public List<string> defaults { get; set; }
+        public ManageFolder()
+        {
+            this.defaults = new List<string>();
+        }
+        //  List<string> defaults = new List<string>(new string[] { });
+
+        public List<string> ViewDefaultFolder(string currentFolder, int layer)
+        {
+            var dirName = "";
+            string dateFormat = "yyyyMMdd";
+            string strBaseDirPath = currentFolder;
+            var existDir = Directory.GetDirectories(strBaseDirPath);
+            DirectoryInfo baseDir = new DirectoryInfo(strBaseDirPath);
+            int inputLayer = layer;
+            inputLayer--;
+
+            if (existDir.Length > 0)
+            {
+                DirectoryInfo[] subDirectories = baseDir.GetDirectories();
+
+                foreach (var folder in subDirectories)
+                {
+                    DateTime dtName;
+                    dirName = folder.Name.Trim();
+                    // Console.WriteLine(dirName);
+                    Console.WriteLine(folder.FullName);
+                    defaults.Add(folder.FullName);
+
+                    if (inputLayer > 0)
+                    {
+                        ViewDefaultFolder(folder.FullName, inputLayer);
+                    }
+                }
+            }
+            var result = defaults;
+            return result;
+        }
+
+        public bool EraseDirectory(string folderPath, List<string> defaultFolder)
+        {
+           // bool recursive = false;
+            //Safety check for directory existence.
+
+            if (!Directory.Exists(folderPath))
+                return false;
+
+            foreach (string file in Directory.GetFiles(folderPath))
+            {
+                Console.WriteLine("Delete file {0}", file);
+                // File.Delete(file);
+            }
+
+            //Console.WriteLine(recursive);
+            if (FilterCollection(folderPath, defaultFolder))
+            {
+                Console.WriteLine("MASUK RECURSIVE");
+                // recursive = true;
+                // if (recursive)
+                {
+                    foreach (string dir in Directory.GetDirectories(folderPath))
+                    {
+                        EraseDirectory(dir, defaultFolder);
+                    }
+                }
+                Console.WriteLine("Delete Directory {0}", folderPath);
+                //Directory.Delete(folderPath);
+            }
+
+
+            //if input == string list folder yang jangan di delete return recursive false --Jadi 
+            // Dia ga akan recursive lagi
+            //Iterate to sub directory only if required.
+            //Delete the parent directory before leaving
+            return true;
+        }
+
+        public static bool FilterCollection(string path, List<string> collection)
+        {
+            bool same = false;
+            foreach (var item in collection)
+            {
+
+                if (path == item)
+                {
+                 Console.WriteLine("{0} SAMA DENGAN {1}", path, path);
+                    same = true;
+                }
+            }
+            // return true;
+            if (same)
+            {
+                return false;
+            }
+            else 
+            {
+                Console.WriteLine("{0} TIDAK SAMA Item di Folder Collection", path);
+                return true;
+            }
+            // return false;
+        }
+
+    }
+}
---
+++ ArchivingFiles/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ArchivingFiles
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir =@"C:\TestAja\" ;
+            // Classs Delete Folder yang memiliki subfolder
+            // Buat class yang berguna untuk delete folder yang memiliki file
+
+            var manageFolder = new ManageFolder();
+            var defaultFolder = manageFolder.ViewDefaultFolder(dir, 2);
+
+            Console.WriteLine("------------------------");
+            //defaultFolder.ForEach(i => Console.Write(string.Join("\t", i)));
+
+            foreach (var item in defaultFolder)
+            {   //Class untuk delete folder yang memiliki file
+                string[] files = Directory.GetFiles(item);
+
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    //    Console.WriteLine(fi.FullName);
+                    if (fi.LastAccessTime < DateTime.Now.AddMonths(-3))
+                        Console.WriteLine(fi);
+                       // fi.Delete();
+                }
+
+                //Split folder(item) yang memiliki 3 \ (garis/path) untuk di delete subfoldernya
+
+                var dirName = "";
+                string dateFormat = "yyyyMMdd";
+                string[] tokens = item.Split("\\");
+                    // Console.WriteLine("-----HASIL SPLIT---------");
+                foreach (var list in tokens)
+                {
+                     Console.WriteLine(list);
+                }
+                   //  Console.WriteLine("-----HASIL SPLIT---------");
+                if (tokens.Length > 3) 
+                {
+                    DirectoryInfo baseDir = new DirectoryInfo(item);
+                    DirectoryInfo[] subDirectories = baseDir.GetDirectories();
+
+                    foreach (var folder in subDirectories)
+                    {
+                        DateTime dtName;
+                        dirName = folder.Name.Trim();
+                       // Console.WriteLine(dirName);
+                        //Console.WriteLine(folder.FullName);
+                          Console.WriteLine("Delete Folder {0}", dirName);
+                        if (DateTime.TryParseExact(dirName, dateFormat, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dtName))
+                        {
+                            if (DateTime.Today.Subtract(dtName).TotalDays > 30)
+                                folder.Delete(true); //to delete all the files and subdirectory inside this directory, recursively
+                        }
+                    }
+                }
+                //class untuk delete folder yang memiliki folder
+               /* DirectoryInfo d = new DirectoryInfo(item);
+                if (d.CreationTime < DateTime.Now.AddDays(-7))
+                        Console.WriteLine(d);
+                    //d.Delete();*/
+
+            }
+
+
+
+            //Console.WriteLine(folder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
+                        if (subDirectories != null && subDirectories.Length > 0)
+                        {
+                            DateTime dtName;
+                            for (int j = subDirectories.Length - 1; j >= 0; j--)
+                            {
+                                Console.WriteLine("gai");
+                                dirName = subDirectories[j].Name.Trim();
+                                Console.WriteLine(dirName);
+                                if (DateTime.TryParseExact(dirName, dateFormat, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out dtName))
+                                {
+                                    if (DateTime.Today.Subtract(dtName).TotalDays > 30)
+                                        // subDirectories[j].Delete(true); //to delete all the files and subdirectory inside this directory, recursively
+                                        Console.WriteLine(dirName);
+                                }
+                            }
+                        }*/
+
+            /*        DirectoryInfo d = new DirectoryInfo(dir);
+                    if (d.CreationTime < DateTime.Now.AddDays(-7))
+                        Console.WriteLine("");
+        */
+        }
+    }
+}
